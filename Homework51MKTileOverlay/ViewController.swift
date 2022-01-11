a02d66e3dd5b56af92ae2f16018e2074de06eb46
Poga Huang <pgh86812@gmail.com>
2022-01-11 11:35:11 +0800

    commit 1

--- Homework51MKTileOverlay/ViewController.swift
+++ Homework51MKTileOverlay/ViewController.swift
@@ -6,14 +6,29 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let template = "http://tile.thunderforest.com/outdoors/{z}/{x}/{y}.png"
+        let overlay = MKTileOverlay(urlTemplate: template)
+        overlay.canReplaceMapContent = true
+        mapView.addOverlay(overlay, level: .aboveLabels)
+        mapView.delegate = self
     }
 
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        if overlay is MKTileOverlay{
+            return MKTileOverlayRenderer(overlay: overlay)
+        }else{
+            return MKTileOverlayRenderer()
+        }
+    }
 
 }
 
